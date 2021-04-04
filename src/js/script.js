3111df4798d1f5bf4a8fbf9e6f5db2c7490fe443
Yaroslav <slideryo@gmail.com>
2021-04-04 18:29:21 +0500

    lesson 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+// Консольное приложение
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+const a = prompt('Один из последних фильмов?',''),
+      b = prompt('на сколько оценитье его?', ''),
+      c = prompt('Один из последних фильмов?',''),
+      d = prompt('на сколько оценитье его?', '');
+
+personalMovieDB.movies[a] = b; // запись через квадратные скобки вместо точки работает корректенее
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
