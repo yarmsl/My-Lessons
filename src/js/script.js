5b3b57b4c39e35f867f411efec5b74ab88fc1331
Yaroslav <slideryo@gmail.com>
2021-04-04 19:33:15 +0500

    version 1.2

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,92 @@
 "use strict";
 // Консольное приложение
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
 
-const personalMovieDB = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres: [],
-   privat: false
-};
+// const personalMovieDB = {
+//    count: numberOfFilms,
+//    movies: {},
+//    actors: {},
+//    genres: [],
+//    privat: false
+// };
 
-const a = prompt('Один из последних фильмов?',''),
-      b = prompt('на сколько оценитье его?', ''),
-      c = prompt('Один из последних фильмов?',''),
-      d = prompt('на сколько оценитье его?', '');
+// for (let i = 0; i < 2; i++) {
+//    const a = prompt('Один из последних фильмов?',''),
+//          b = prompt('на сколько оценитье его?', '');
+         
+//          if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b; 
+//             console.log('everthng ok');
+//          } else {
+//             console.log('err');
+//             i--;
+//          }    
+// }
 
-personalMovieDB.movies[a] = b; // запись через квадратные скобки вместо точки работает корректенее
-personalMovieDB.movies[c] = d;
+// if (personalMovieDB.count < 10) {
+//    console.log('Просмотрено очень мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//    console.log('Вы классический')
+// } else if (personalMovieDB.count >= 30) {
+//    console.log('Вы киноман');
+// } else {
+//    console.log('Ошибка!')
+// }
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// Условия 
+// if (4 == 8) {
+//    console.log('ok');
+// } else {
+//    console.log('er');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//    console.log('err');
+// } else if (num > 100) {
+//    console.log('too much')
+// } else {
+//    console.log('yes')
+// };
+
+// (num === 50) ? (console.log('ok')) : (console.log('err'));
+
+// switch (num) {
+//    case 49:
+//       console.log('nope');
+//       break;
+//    case 100:
+//       console.log('nope');
+//       break;
+//    case 50:
+//       console.log('ye');
+//       break;
+//    default: 
+//       console.log('dflt');
+//       break;
+// }
+
+// Циклы
+// let num = 50;
+
+// while (num < 55) {
+//    console.log(num);
+//    num++;
+// };
+
+// do {
+//    console.log(num);
+//    num++;
+// }
+// while (num <55);
+
+// for (let i = 1; i < 10; i++) {
+//    if (i === 6) {
+//       // break;
+//       continue;
+//    }
+//    console.log(i);
+// }
